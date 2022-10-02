922a93a2318839f7dbfd40ffd7379d203e436493
Luis E. Villavicencio Torres <khanccer@gmail.com>
2022-10-01 19:03:50 -0500

    fullstack phonebook 1

---
+++ models/person.js
@@ -0,0 +1,47 @@
+
+const { mongoose, model, Schema} = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to ', url)
+
+mongoose.connect(url)
+  .then( result => {
+    console.log('connecting to MongoDB')
+  })
+  .catch( err => {
+    console.log('error to connecting to MongoDB', err.message);
+  })
+
+const personSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    trim: true,
+    unique: true
+  },
+  number: { 
+    type: String,
+    required: true,
+    minlength: 8,
+    trim: true,
+    unique: true
+  }
+})
+
+personSchema.plugin(uniqueValidator)
+
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+
+module.exports = model('Person', personSchema)
+
---
+++ mongo.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+
+console.log(process.argv.length)
+
+if (process.argv.length < 3 || 
+  (process.argv.length === 4 || process.argv.length > 5)) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2];
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.tucqqyh.mongodb.net/persons?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+        .catch( err => { console.log(err)})
+
+mongoose.connection.on("error", (err)=> {
+  console.log(err);
+})
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String  
+})
+
+const Person = mongoose.model('Person', personSchema, 'persons')
+
+if (process.argv.length !== 3) {
+
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+  
+  person.save().then(result => {
+    console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+    mongoose.connection.close()
+  })
+  
+}
+else {
+  Person
+  .find({})
+  .then( result => {
+    console.log('phonebook:\n')
+    result.forEach( person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+    process.exit(0)  
+  }) 
+}  
+
+
+
+
